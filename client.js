030b20027c0f99da33bc5c89a8f6ec6a33602059
McKynlee Westman <mckynlee.westman@gmail.com>
2021-01-28 13:51:42 -0600

    adding jQuery commands

--- client.js
+++ client.js
@@ -6,5 +6,15 @@ function onReady() {
   console.log('so ready');
 
   const h1 = $('h1');
-  console.log('h1');
+  console.log('h1', h1);
+
+  const li = $('Li');
+  console.log('li', li);
+  // make it blue
+  li.css('background-color', 'teal');
+
+  // $('h1').css('background-color', 'green');
+  // OR:
+  $('h1').addClass('froggy');
+  // can .toggleClass to turn it off and on with clicks
 }
